fe4e882c655e90b67e38c17ff159bf41ba7c439a
Gerald Schuberth-Hlavač <abuseki@synapticgap.com>
2022-03-07 15:15:45 +0100

    * removed some skeletons from det data directory * renaming of ContinentalCrust data sets

--- tests/testthat/testDatasetHealth.R
+++ tests/testthat/testDatasetHealth.R
@@ -4,9 +4,9 @@ test_that('No NAs in datasets', {
   expect_false(any(is.na(CI__McDonough_Sun__1995)))
   expect_false(any(is.na(Pyrolite__McDonough_Sun__1995)))
 
-  expect_false(any(is.na(UpperCC__Taylor_McLennan__1995)))
-  expect_false(any(is.na(BulkCC__Taylor_McLennan__1995)))
-  expect_false(any(is.na(LowerCC__Taylor_McLennan__1995)))
+  expect_false(any(is.na(CC_Upper__Taylor_McLennan__1995)))
+  expect_false(any(is.na(CC_Bulk__Taylor_McLennan__1995)))
+  expect_false(any(is.na(CC_Lower__Taylor_McLennan__1995)))
 
   expect_false(any(is.na(ALL_MORB__GALE__2013)))
   expect_false(any(is.na(BAB__GALE__2013)))
