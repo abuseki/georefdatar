1bd39ab3a7e49b99c010d79125b7da61b63c383c
Schuberth <abuseki@synapticgap.com>
2026-04-10 16:49:41 +0200

    Improved documentation for  improve documentation for DM__Salters_Stracke__2004

--- R/DM__Salters_Stracke__2004.R
+++ R/DM__Salters_Stracke__2004.R
@@ -1,9 +1,14 @@
 #' Depleted mantle
 #'
+#' @description
 #' A data set containing the estimated composition of the Depleted Mantle as
 #' given by \insertCite{Salters2004;textual}{georefdatar}.
 #'
-#' Please refer to the paper for the units.
+#' @details
+#' The data set contains the elemnts and their values. Here is a table that
+#' also contains the elements, their modeled values and their units:
+#' `r knitr::kable(read.csv('data-raw/DM__Salters_Stracke__2004.csv', comment.char = '#')[c('Element', 'Model', 'Unit')], format="markdown")`
+#'
 #'
 #' @format A data frame with 1 row and `r ncol(DM__Salters_Stracke__2004)` element concentrations in ppm:\cr
 #' `r paste(names(DM__Salters_Stracke__2004), collapse= ', ')`
